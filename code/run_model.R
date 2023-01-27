a0f9ad68785e1f3b9543116bc96bcb1dcc990fc2
SimonCMills <smills2@sheffield.ac.uk>
2023-01-27 16:55:22 +0000

    minor tidying

--- code/run_model.R
+++ code/run_model.R
@@ -1,20 +1,37 @@
 # fit models
-library(dplyr); library(flocker); library(mgcv)
+library(dplyr); library(flocker)
 
-# datasets
+# datasets ----
 bird_data <- readRDS("data/paramo_bird_dataset.rds")
+
+# vegetation data
 veg_data <- bird_data %>%
-    select(point, elev_ALOS, habitat_sc, habitat_type, site, cluster, 
+    select(point, elev_ALOS, habitat_type, site, cluster, 
            abu_espeletia, abu_shrub) %>%
     unique %>%
     mutate(elev_sc = scale(elev_ALOS))
+
+# data summary ----
+bird_data %>%
+    ungroup %>%
+    select(habitat_type, point) %>%
+    unique %>%
+    group_by(habitat_type) %>%
+    summarise(n())
+
+bird_data %>%
+    ungroup %>%
+    select(species) %>%
+    unique %>%
+    nrow
+
 
 # format data for occupancy model ----
 det <- select(bird_data, v1:v4) %>%
     mutate_all(.funs = function(x)ifelse(x==-99, NA, x)) %>% 
     as.matrix
 
-cov <- select(bird_data, species, phylo = species_eltontraits, 
+cov <- select(bird_data, species, phylo, 
               abu_espeletia, abu_shrub, 
               elev_ALOS, relev, site, cluster, habitat_type, point) %>%
     mutate(sp_cl = interaction(species, cluster), 
@@ -51,13 +68,14 @@ f_det <- ~ 1 + abu_espeletia_sc + abu_shrub_sc + time + (1 + time |species) +
 # fit occupancy ----
 flocker_fit <- flock(f_occ, f_det, fd, rep_constant = F, 
                      backend = "cmdstanr", cores = 4, chains=4, 
-                     file = "outputs/flocker_fit.rds")
+                     file = "outputs/flocker_fit2.rds")
 
 # fit vegetation ----
 fit_esp <- brms::brm(bf(abu_espeletia ~ 1 + elev_sc + habitat_sc + site, 
                         zi ~ elev_sc), 
                      veg_data, adapt_delta = .99,
-                     backend = "cmdstanr", family="zero_inflated_negbinomial",
+                     backend = "cmdstanr", 
+                     family="zero_inflated_negbinomial",
                      chains = 4, cores = 4, 
                      file = "outputs/espeletia_fit.rds",
                      prior = c(set_prior("normal(0, 3)", "b")))
@@ -65,7 +83,8 @@ fit_esp <- brms::brm(bf(abu_espeletia ~ 1 + elev_sc + habitat_sc + site,
 fit_shr <- brms::brm(bf(abu_shrub ~ 1 + elev_sc + habitat_sc + site, 
                         zi ~ elev_sc), 
                      veg_data, adapt_delta = .99,
-                     backend = "cmdstanr", family="zero_inflated_negbinomial",
+                     backend = "cmdstanr", 
+                     family="zero_inflated_negbinomial",
                      chains = 4, cores = 4, 
                      file = "outputs/shrub_fit.rds",
                      prior = c(set_prior("normal(0, 3)", "b")))
